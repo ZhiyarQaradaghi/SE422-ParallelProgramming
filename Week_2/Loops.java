17e2c99ff00c3bd1a853556d292fafee017ffc94
Zhiyar_Qaradaghi <zhiyar.burhan2003@gmail.com>
2025-02-20 22:10:35 +0300

    first commit

---
+++ Week_2/Loops.java
@@ -0,0 +1,14 @@
+public class Loops {
+    public static void main(String[] args) {
+        while(true) {
+            System.out.println("Hello from SE422");
+        }
+
+        // while (true) { 
+        //     System.out.println("Hello from ITE306");
+            
+        // }
+    }
+}
+
+// Silicon Lotery 
