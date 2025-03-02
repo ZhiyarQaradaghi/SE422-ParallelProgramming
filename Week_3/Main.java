4d7c15e28f655b4a6e9408336504406c20a5e29c
Zhiyar_Qaradaghi <zhiyar.burhan2003@gmail.com>
2025-03-02 10:11:55 +0300

    Week 4 lecture 1 - Context Switching

--- Week_3/Main.java
+++ Week_3/Main.java
@@ -2,5 +2,5 @@ package Week_3;
 
 // ref: andrews chapter 1
 public class Main {
-    
+        
 }
---
+++ Week_4/ThreadPoolExample.java
@@ -0,0 +1,26 @@
+package Week_4;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+// Fixed Thread Pool
+public class ThreadPoolExample {
+    public static void main(String[] args) {
+        int cores = Runtime.getRuntime().availableProcessors();
+        ExecutorService pool = Executors.newFixedThreadPool(cores+1); // why cores + 1 is faster than cores? If you keep it cores, sometimes where you have a task is very short and finish quick - between the time this core is free and until it gets another - this time is wasted 
+        // with core + 1, once the core finsihes a task quickly and is free, it will immediately get the next task 
+        
+        // only 3 threads, no less no more threads - maximum number of threads that this pool is going to use 
+        // 3 threads are created and are waiting in idle 
+
+        for(int iq=0; iq < 100000; iq++) {
+            Runnable task = () -> {
+            for(int i=0; i < 100000; i++) {
+                System.out.println("AUIS");
+            }
+        };
+        pool.execute(task);
+    }
+    }    
+}
